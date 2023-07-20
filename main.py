2ea66b768e5eaaa0af7a707e63ec1c79fece8bce
sudhaunsh <sudhaunsh26@gmail.com>
2023-07-20 18:06:26 +0100

    modified:   main.py 	new file:   predictions.atom 	renamed:    templates/assets/atomlogo.png -> static/atomlogo.png 	renamed:    templates/assets/denvlogo.png -> static/denvlogo.png 	modified:   templates/denv.html 	modified:   templates/home.html 	modified:   templates/result_denv.html

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask 
 from login import*
-from denv_back_end import*
+from denV.back_end import*
+from denV.advisory import*
 
 
 app = flask.Flask(__name__)
@@ -37,12 +38,13 @@ def use_denv():
         verify_val = {0:flask.request.form['nm'],1:flask.request.form['Counta'],2:flask.request.form['Count_b'],3:flask.request.form['Age'],4:0,5:flask.request.form['Date_a'],6:flask.request.form['Date_b']}
         denv_result = estimate(verify_val)
         lc = denv_result[3]
+        level = np.min(lc)
+        advice = advise(level)
         hc = denv_result[2]
         confidence = denv_result[4]
-        f=open('denv_result.txt','w')
         name = str(verify_val[0])
         age = str(verify_val[3])
-        return flask.render_template('result_denv.html',Name = name, Age = age, Date_onset = date_onset,LC1 = str(lc[0]),LC2 = str(lc[1]),LC3 = str(lc[2]),LC4 = str(lc[3]),LC5 = str(lc[4]),LC6 = str(lc[5]),LC7 = str(lc[6]),LC8 = str(lc[7]),LC9 = str(lc[8]),LC10 = str(lc[9]),LC11 = str(lc[10]),HC1 = str(hc[0]),HC2 = str(hc[1]),HC3 = str(hc[2]),HC4 = str(hc[3]),HC5 = str(hc[4]),HC6 = str(hc[5]),HC7 = str(hc[6]),HC8 = str(hc[7]),HC9 = str(hc[8]),HC10 = str(hc[9]),HC11 = str(hc[10]),Confidence = str(confidence))
+        return flask.render_template('result_denv.html',Name = name, Age = age, Date_onset = date_onset,LC1 = str(lc[0]),LC2 = str(lc[1]),LC3 = str(lc[2]),LC4 = str(lc[3]),LC5 = str(lc[4]),LC6 = str(lc[5]),LC7 = str(lc[6]),LC8 = str(lc[7]),LC9 = str(lc[8]),LC10 = str(lc[9]),LC11 = str(lc[10]),HC1 = str(hc[0]),HC2 = str(hc[1]),HC3 = str(hc[2]),HC4 = str(hc[3]),HC5 = str(hc[4]),HC6 = str(hc[5]),HC7 = str(hc[6]),HC8 = str(hc[7]),HC9 = str(hc[8]),HC10 = str(hc[9]),HC11 = str(hc[10]),Confidence = str(confidence),Advice = advice)
         
     return flask.render_template('use_denv.html')
 
